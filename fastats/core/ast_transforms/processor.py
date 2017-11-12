4294543a1e847ef1d6b3d8caee75395a03bbd585
Dave Willmer <dave.willmer@gmail.com>
2017-11-12 10:55:45 +0000

    Fix IndentationError issues with nested functions

--- fastats/core/ast_transforms/processor.py
+++ fastats/core/ast_transforms/processor.py
@@ -23,8 +23,13 @@ class AstProcessor:
         self._debug = self._overrides.get('debug')
 
     def process(self):
-        source = inspect.getsource(self.top_level_func)
-        tree = ast.parse(source)
+        try:
+            source = inspect.getsource(self.top_level_func)
+            tree = ast.parse(source)
+        except IndentationError:
+            data = uncompile(self.top_level_func.__code__)
+            tree = parse_snippet(*data)
+
         globs = self.top_level_func.__globals__
         globs['jit'] = jit
         t = CallTransform(self._overrides, globs, self._replaced, self._new_funcs)
@@ -75,3 +80,46 @@ class AstProcessor:
                 fixnames(c.co_varnames), c.co_filename, c.co_name,
                 c.co_firstlineno, c.co_lnotab, c.co_freevars, c.co_cellvars)
         return c
+
+
+def uncompile(c):
+    """ uncompile(codeobj) -> [source, filename, mode, flags, firstlineno, privateprefix] """
+    # if c.co_flags & inspect.CO_NESTED or c.co_freevars:
+    #     raise Exception('nested functions not supported')
+    if c.co_name == '<lambda>':
+        raise Exception('lambda functions not supported')
+    if c.co_filename == '<string>':
+        raise Exception('code without source file not supported')
+
+    filename = inspect.getfile(c)
+    try:
+        lines, firstlineno = inspect.getsourcelines(c)
+    except IOError:
+        raise Exception('source code not available')
+    source = ''.join(lines)
+
+    # __X is mangled to _ClassName__X in methods. Find this prefix:
+    privateprefix = None
+    for name in c.co_names:
+        m = re.match('^(_[A-Za-z][A-Za-z0-9_]*)__.*$', name)
+        if m:
+            privateprefix = m.group(1)
+            break
+
+    return [source, filename, 'exec', c.co_flags, firstlineno, privateprefix]
+
+
+def parse_snippet(source, filename, mode, flags, firstlineno, privateprefix_ignored=None):
+    """ Like ast.parse, but accepts indented code snippet with a line number offset. """
+    args = filename, mode, ast.PyCF_ONLY_AST, True
+    prefix = '\n'
+    try:
+        a = compile(prefix + source, *args)
+    except IndentationError:
+        # Already indented? Wrap with dummy compound statement
+        prefix = 'with 0:\n'
+        a = compile(prefix + source, *args)
+        # peel wrapper
+        a.body = a.body[0].body
+    ast.increment_lineno(a, firstlineno - 2)
+    return a
