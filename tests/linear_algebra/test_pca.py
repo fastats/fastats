2ee605e73b638a4a791f96a7bdea9bcf9cf7c12f
Rob Ennis <rjenc29@gmail.com>
2017-12-03 20:25:39 +0000

    update tests

--- tests/linear_algebra/test_pca.py
+++ tests/linear_algebra/test_pca.py
@@ -8,29 +8,26 @@ from fastats.linear_algebra import pca
 from tests.data.datasets import SKLearnDataSets
 
 
-@mark.parametrize('A', SKLearnDataSets)
-def test_pca_sklearn(A):
-    data = A.value.data
-
+def check_versus_sklearn(data, fn):
     for n in range(1, data.shape[1] + 1):
         sk_pca = PCA(n_components=n)
         sk_pca.fit(data)
         expected = sk_pca.transform(data)
-        output = pca(data, components=n)
+        output = fn(data, components=n)
         assert np.allclose(np.abs(expected), np.abs(output))  # vector could legitimately be in 'opposite' direction
+
+
+@mark.parametrize('A', SKLearnDataSets)
+def test_pca_sklearn(A):
+    data = A.value.data
+    check_versus_sklearn(data, pca)
 
 
 @mark.parametrize('A', SKLearnDataSets)
 def test_pca_jit_sklearn(A):
     data = A.value.data
     pca_jit = jit(pca)
-
-    for n in range(1, data.shape[1] + 1):
-        sk_pca = PCA(n_components=n)
-        sk_pca.fit(data)
-        expected = sk_pca.transform(data)
-        output = pca_jit(data, components=n)
-        assert np.allclose(np.abs(expected), np.abs(output))  # vector could legitimately be in 'opposite' direction
+    check_versus_sklearn(data, pca_jit)
 
 
 if __name__ == '__main__':
