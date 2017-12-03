9edcdcf548b011126fa24863cd3ef0143ce7b006
Rob Ennis <rjenc29@gmail.com>
2017-12-03 20:04:05 +0000

    generalise PCA tests - add jit test back in

--- tests/linear_algebra/test_pca.py
+++ tests/linear_algebra/test_pca.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from numba import jit
 from pytest import mark
 from sklearn.decomposition import PCA
 
@@ -16,6 +17,19 @@ def test_pca_sklearn(A):
         sk_pca.fit(data)
         expected = sk_pca.transform(data)
         output = pca(data, components=n)
+        assert np.allclose(np.abs(expected), np.abs(output))  # vector could legitimately be in 'opposite' direction
+
+
+@mark.parametrize('A', SKLearnDataSets)
+def test_pca_jit_sklearn(A):
+    data = A.value.data
+    pca_jit = jit(pca)
+
+    for n in range(1, data.shape[1] + 1):
+        sk_pca = PCA(n_components=n)
+        sk_pca.fit(data)
+        expected = sk_pca.transform(data)
+        output = pca_jit(data, components=n)
         assert np.allclose(np.abs(expected), np.abs(output))  # vector could legitimately be in 'opposite' direction
 
 
