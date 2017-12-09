552d75731cdc73dd28e87987664063d34c22bcca
Dave Willmer <dave.willmer@gmail.com>
2017-12-09 18:04:09 +0000

    Remove unused context manager

--- fastats/core/decorator.py
+++ fastats/core/decorator.py
@@ -1,5 +1,4 @@
 
-from contextlib import contextmanager
 from functools import wraps
 from inspect import isfunction
 
@@ -7,18 +6,7 @@ from fastats.core.ast_transforms.convert_to_jit import convert_to_jit
 from fastats.core.ast_transforms.processor import AstProcessor
 
 
-@contextmanager
-def code_transform(func, replaced):
-    try:
-        yield func
-    finally:
-        # TODO : remove this context manager as it's now not needed?
-        pass
-
-
 def fs(func):
-    # The initial function *must* be jittable,
-    # else we can't do anything.
     _func = func
     replaced = {}
 
@@ -38,30 +26,28 @@ def fs(func):
         if not kwargs:
             return _func(*args)
 
-        with code_transform(_func, replaced) as _f:
-            # TODO : remove fastats keywords such as 'debug'
-            # before passing into AstProcessor
-            new_funcs = {}
-            for v in kwargs.values():
-                if isfunction(v) and v.__name__ not in kwargs:
-                    inner_replaced = {}
-                    with code_transform(v, inner_replaced) as g:
-                        processor = AstProcessor(g, kwargs, inner_replaced, new_funcs)
-                        proc = processor.process()
-                        new_funcs[v.__name__] = convert_to_jit(proc)
+        # TODO : remove fastats keywords such as 'debug'
+        # before passing into AstProcessor
+        new_funcs = {}
+        for v in kwargs.values():
+            if isfunction(v) and v.__name__ not in kwargs:
+                inner_replaced = {}
+                processor = AstProcessor(v, kwargs, inner_replaced, new_funcs)
+                proc = processor.process()
+                new_funcs[v.__name__] = convert_to_jit(proc)
 
-            new_kwargs = {}
-            for k, v in kwargs.items():
-                if new_funcs.get(v.__name__):
-                    new_kwargs[k] = new_funcs[v.__name__]
-            kwargs.update(new_kwargs)
+        new_kwargs = {}
+        for k, v in kwargs.items():
+            if new_funcs.get(v.__name__):
+                new_kwargs[k] = new_funcs[v.__name__]
+        kwargs.update(new_kwargs)
 
-            processor = AstProcessor(_f, kwargs, replaced, new_funcs)
-            proc = processor.process()
-            if return_callable:
-                return convert_to_jit(proc)
+        processor = AstProcessor(_func, kwargs, replaced, new_funcs)
+        proc = processor.process()
+        if return_callable:
+            return convert_to_jit(proc)
 
-            return convert_to_jit(proc)(*args)
+        return convert_to_jit(proc)(*args)
 
     fs_wrapper.undecorated = _func
     return fs_wrapper
