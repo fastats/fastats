4b694104104ef2217f1c55398460afd8b149af4e
Dave Willmer <dave.willmer@gmail.com>
2017-12-11 23:39:11 +0000

    Move `ols` from `fastats.maths` to `fastats.linear_algebra`

--- fastats/linear_algebra/__init__.py
+++ fastats/linear_algebra/__init__.py
@@ -1,9 +1,31 @@
 
 from fastats.linear_algebra.lu import lu, lu_inplace
+from fastats.linear_algebra.ols import (
+    ols, ols_cholesky, ols_qr, ols_svd, r_squared,
+    sum_of_squared_residuals, fitted_values, residuals,
+    adjusted_r_squared, standard_error, t_statistic,
+    mean_standard_error_residuals, add_intercept,
+    adjusted_r_squared_no_intercept, r_squared_no_intercept
+)
 from fastats.linear_algebra.pca import pca
 
 __all__ = [
     'lu',
     'lu_inplace',
+    'ols',
+    'ols_cholesky',
+    'ols_qr',
+    'ols_svd',
     'pca',
+    'add_intercept',
+    'r_squared',
+    'r_squared_no_intercept',
+    'sum_of_squared_residuals',
+    'fitted_values',
+    'residuals',
+    'adjusted_r_squared',
+    'adjusted_r_squared_no_intercept',
+    'standard_error',
+    'mean_standard_error_residuals',
+    't_statistic',
 ]
