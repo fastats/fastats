4d5908844b72f217c86fcfb34183532ad8b4a365
Paweł Romanowski <pawroman@gmail.com>
2017-12-13 17:05:14 +0000

    Add stateful rolling window

---
+++ fastats/core/windowed_pass.py
@@ -0,0 +1,75 @@
+
+import numpy as np
+
+from fastats.core.decorator import fs
+
+
+def value(x):
+    return x[0]
+
+
+@fs
+def windowed_stateful_pass(x, win):
+    """
+    Performs a *stateful* rolling (windowed)
+    iteration over the first dimension of `x`.
+
+    This allows to improve performance by avoiding
+    re-calculating values from scratch for each
+    window.
+
+    This has the exact same properties as
+    :func:`windowed_pass`, but expects the
+    ``value`` function be "stateful" and have this
+    return signature:
+    ``(window_arr, val_in, val_out, state_arr)``
+
+    ``val_in`` and ``val_out`` are the values
+    coming "in" to the window and already thrown
+    "out" of the window in each iteration.
+
+    The state starts off as empty array, then each
+    successful return from the ``value`` function
+    sets the state by returning it along the
+    window's value.  This means that the function
+    has exclusive control over the ``state``'s
+    contents.
+
+    Example
+    -------
+    >>> import numpy as np
+    >>> def rolling_sum(x, val_in, val_out, state):
+    ...     if state.size == 0:   # state is empty
+    ...         state = np.array([np.sum(x)], dtype=x.dtype)
+    ...     else:
+    ...         state[0] += val_in - val_out
+    ...     return state[0], state
+    >>> x = np.arange(7, dtype='float')
+    >>> result = windowed_stateful_pass(x, 4, value=rolling_sum)
+    >>> result
+    array([ nan,  nan,  nan,   6.,  10.,  14.,  18.])
+    """
+    result = np.full_like(x, np.nan)
+    state = np.empty(0, dtype=result.dtype)
+
+    # must call value to get the state
+    val_in = x[win-1]
+    val_out = np.nan
+    result[win-1], state = value(x[0:win], val_in, val_out, state)
+
+    # subsequent iterations can ignore state assignment
+    # (without this, numba won't optimize out the assignment)
+    for i in range(win+1, x.shape[0]+1):
+        start = i - win
+        in_idx = i - 1
+
+        val_in = x[in_idx]
+        val_out = x[start-1]
+        result[in_idx], _ = value(x[start:i], val_in, val_out, state)
+
+    return result
+
+
+if __name__ == '__main__':
+    import pytest
+    pytest.main()
---
+++ tests/core/test_windowed_stateful_pass.py
@@ -0,0 +1,63 @@
+
+import numpy as np
+from numpy.testing import assert_allclose
+
+from fastats import windowed_stateful_pass
+
+
+def test_windowed_stateful_pass_respects_value():
+    def double(x, val_in, val_out, state):
+        return val_in * 2, state
+
+    data = np.array(range(101), dtype='float') ** 3
+    ret = windowed_stateful_pass(data, 7, value=double)
+
+    assert np.isnan(ret[0:6]).all()
+    assert_allclose(ret[6:], data[6:] * 2)
+
+
+def test_windowed_stateful_pass_val_out():
+    def out_flip(x, val_in, val_out, state):
+        return val_out * -1, state
+
+    data = np.array(range(95), dtype='float') ** 3
+    ret = windowed_stateful_pass(data, 9, value=out_flip)
+
+    assert np.isnan(ret[0:8]).all()
+    assert_allclose(ret[9:], data[:-9] * -1)
+
+
+def test_windowed_stateful_pass_constant():
+    def mean_range(x, val_in, val_out, state):
+        state = np.empty(5, dtype=x.dtype)
+        state[:] = [1, 2, 3, 4, 5]
+
+        return state.mean(), state
+
+    data = np.array(range(50), dtype='float')
+    ret = windowed_stateful_pass(data, 4, value=mean_range)
+
+    assert np.isnan(ret[0:3]).all()
+    assert_allclose(ret[3:], 3.0)
+
+
+def test_windowed_stateful_pass_rolling_mean():
+    def rolling_mean(x, val_in, val_out, state):
+        if state.size == 0:
+            window_sum = np.sum(x)
+            state = np.array([window_sum])
+        else:
+            state[0] += val_in - val_out
+
+        return state[0] / len(x), state
+
+    data = np.array([1, 2, 3, 5, 8, 13], dtype='float')
+    ret = windowed_stateful_pass(data, 2, value=rolling_mean)
+
+    assert np.isnan(ret[0])
+    assert_allclose(ret[1:], np.array([1.5, 2.5, 4.0, 6.5, 10.5]))
+
+
+if __name__ == '__main__':
+    import pytest
+    pytest.main([__file__])
