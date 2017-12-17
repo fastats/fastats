dacb15da95c9498a94e6156d319ce058bed56dff
Dave Willmer <dave.willmer@gmail.com>
2017-12-17 10:05:31 +0000

    Add minimal repro from numba#2609 for threading issue

---
+++ tests/test_numba_parallel_issues.py
@@ -0,0 +1,34 @@
+
+from hypothesis import given
+from hypothesis.strategies import integers
+
+from numba import jit
+import numpy as np
+
+
+@jit(nopython=True, parallel=True)
+def get(n):
+    return np.ones((n,1), dtype=np.float64)
+
+
+@given(integers(min_value=10, max_value=100000))
+def test_all_ones(x):
+    """
+    We found one of the scaling tests failing on
+    OS X with numba 0.35, but it passed on other
+    platforms, and passed consistently with numba
+    0.36.
+
+    The issue appears to be the same as numba#2609
+    https://github.com/numba/numba/issues/2609
+    so we've taken the minimal repro from that issue
+    and are using it as a unit-test here.
+    """
+    result = get(x)
+    expected = np.ones((x, 1), dtype=np.float64)
+    assert np.allclose(expected, result)
+
+
+if __name__ == '__main__':
+    import pytest
+    pytest.main([__file__])
