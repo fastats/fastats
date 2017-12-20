722f1f2cb6653ce8636e86ae853bba61a3a497e2
Aaron Critchley <aaron.critchley@gmail.com>
2017-12-20 23:29:06 +0000

    Add failing test and standard python version for illustration

--- tests/core/test_decorator.py
+++ tests/core/test_decorator.py
@@ -1,7 +1,9 @@
 
+import pytest
 from fastats.core.decorator import fs
 
 
+@pytest.mark.xfail
 def test_decorated_func_kwargs():
     @fs
     def square(x):
@@ -11,9 +13,19 @@ def test_decorated_func_kwargs():
     def func_that_takes_func_kwarg(arg, func=abs):
         return func(arg)
 
-    func_that_takes_func_kwarg(4, func=square)
+    result = func_that_takes_func_kwarg(4, func=square)
+    assert result == 16
 
+
+def test_no_decorator_func_kwargs():
+    def square(x):
+        return x ** 2
+
+    def func_that_takes_func_kwarg(arg, func=abs):
+        return func(arg)
+
+    result = func_that_takes_func_kwarg(4, func=square)
+    assert result == 16
 
 if __name__ == '__main__':
-    import pytest
     pytest.main([__file__])
