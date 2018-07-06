f295b50b00cf8d8a88f3da75f8d0549280ede25f
Aaron Critchley <aaron.critchley@gmail.com>
2018-07-06 23:09:32 +0100

    Adding benchamrks

--- fastats/maths/ewma.py
+++ fastats/maths/ewma.py
@@ -6,7 +6,7 @@ def ewma(x, halflife):
     """
     Exponential weighted moving average
     """
-    factor = np.exp(np.log(0.5) / halflife)
+    decay_coefficient = np.exp(np.log(0.5) / halflife)
     out = np.empty_like(x, dtype=np.float64)
 
     for i in range(out.shape[0]):
@@ -14,11 +14,12 @@ def ewma(x, halflife):
             out[i, :] = x[i, :]
             sum_prior = 1
         else:
-            sum_i = sum_prior + np.power(factor, i)
-            out[i, :] = (factor * out[i - 1, :] * sum_prior + x[i, :]) / sum_i
+            sum_i = sum_prior + np.power(decay_coefficient, i)
+            out[i, :] = (decay_coefficient * out[i - 1, :] * sum_prior + x[i, :]) / sum_i
             sum_prior = sum_i
 
     return out
+
 
 if __name__ == '__main__':
     import pytest
