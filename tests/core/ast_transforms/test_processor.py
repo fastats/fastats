c3059b2f394a14c18be89a370889080b5c659aae
Aaron Critchley <aaron.critchley@gmail.com>
2018-07-27 16:31:54 +0100

    Adding tests for processor

---
+++ tests/core/ast_transforms/test_processor.py
@@ -0,0 +1,57 @@
+
+import pytest
+import ast
+from fastats.core.ast_transforms.processor import recompile, uncompile
+
+
+def test_recompile_happy_path():
+    func_as_string = '''
+def f(n):
+     # This is a comment
+     return n ** 2
+    '''
+    tree_module = ast.parse(func_as_string)
+    recompile(tree_module, 'test_processor.py', 'exec')
+
+
+def test_recompile_no_func():
+    not_a_func = '''
+12 + 12 * 3
+'''
+    tree_module = ast.parse(not_a_func)
+    with pytest.raises(TypeError, match='Function body code not found'):
+        recompile(tree_module, 'test_processor.py', 'exec')
+
+
+def test_uncompile_happy_path():
+    def sq(num):
+        return num ** 2
+    result = uncompile(sq.__code__)
+    try:
+        iter(result)
+    except Exception as err:
+        raise Exception('''
+        Expected the result of uncompile to be iterable, error below:
+        {}'''.format(err))
+
+
+def test_uncompile_lambdas():
+    lam_square = lambda x: x ** 2
+    with pytest.raises(TypeError, match='lambda functions not supported'):
+        uncompile(lam_square.__code__)
+
+
+def test_uncompile_string():
+    compiled = compile('21 + 21', '<string>', 'exec')
+    with pytest.raises(ValueError, match='code without source file not supported'):
+        uncompile(compiled)
+
+
+def test_uncompile_bad_file_path():
+    compiled = compile('21 + 21', '/this/file/doesnt/exist.py', 'exec')
+    with pytest.raises(Exception, match='source code not available'):
+        uncompile(compiled)
+
+
+if __name__ == 'main':
+    pytest.main([__file__])
