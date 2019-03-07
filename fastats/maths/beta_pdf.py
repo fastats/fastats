673314daf7670902da0e70cc297576333237ca96
ktattan <killiantattan@gmail.com>
2019-03-07 19:17:00 +0000

    using new gammaln function

--- fastats/maths/beta_pdf.py
+++ fastats/maths/beta_pdf.py
@@ -1,6 +1,6 @@
 
-from numpy import power
-from fastats.maths.gamma import gamma
+from numpy import power, exp
+from fastats.maths.gamma import gammaln
 
 
 def beta_pdf(x, alpha, beta):
@@ -13,15 +13,16 @@ def beta_pdf(x, alpha, beta):
     `alpha` and `beta`.
 
     >>> beta_pdf(0.56, 1.2, 3.4) # doctest: +ELLIPSIS
-    0.606886294679549...
-    >>> beta_pdf(0.2, 2, 2)
-    0.9600000335418806
-    >>> beta_pdf(0.8, 2, 2)
-    0.9600000335418805
+    0.60688628807551...
+    >>> beta_pdf(0.2, 2, 2) # doctest: +ELLIPSIS
+    0.95999999999999...
+    >>> beta_pdf(0.8, 2, 2) # doctest: +ELLIPSIS
+    0.95999999999999...
     """
-    u = gamma(alpha + beta) * power(1 - x, beta - 1) * power(x, alpha - 1)
-    v = gamma(alpha) * gamma(beta)
-    out = u / v
+    u = gammaln(alpha + beta) - gammaln(alpha) - gammaln(beta)
+    u = exp(u)
+    v = power(x, alpha - 1) * power(1 - x, beta - 1)
+    out = u * v
     return out
 
 
